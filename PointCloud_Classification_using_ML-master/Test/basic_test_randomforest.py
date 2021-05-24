2728838bc820012a3062c157c80480abb6ac134f
HuangCongQing <1756260160@qq.com>
2021-05-24 09:52:57 +0800

    fix

--- PointCloud_Classification_using_ML-master/Test/basic_test_randomforest.py
+++ PointCloud_Classification_using_ML-master/Test/basic_test_randomforest.py
@@ -4,7 +4,7 @@ Author: HCQ
 Company(School): UCAS
 Email: 1756260160@qq.com
 Date: 2021-05-23 18:53:02
-LastEditTime: 2021-05-24 09:37:01
+LastEditTime: 2021-05-24 09:52:50
 FilePath: /sklearn/PointCloud_Classification_using_ML-master/Test/basic_test_randomforest.py
 '''
 # encoding=utf-8
@@ -38,8 +38,9 @@ rfc = joblib.load(MODEL_DIR)
 
 # prediction / test 得到各种参数  accuracy，confusion，precision，log
 y_pred = rfc.predict(data)
+print("y_pred: ", y_pred)
 score = metrics.accuracy_score(target, y_pred) # 得到score
-print('accuracy score = ', score)
+print('accuracy score = ', score) # accuracy score =  0.9999622313706236
 conf_matrix = metrics.confusion_matrix(target, y_pred, [0,1,2,3,4])
 print('confusion matrix = ')
 print(conf_matrix)
@@ -49,6 +50,7 @@ precision = metrics.precision_score(target, y_pred, average='weighted')
 print('precision score = ', precision)
 f1 = metrics.f1_score(target, y_pred, average='weighted')
 print('f1 score = ', f1)
-prob = rfc.predict_proba(data)
-log_loss = metrics.log_loss(target, prob, labels=np.array([0,1,2,3,4]))
+prob = rfc.predict_proba(data) #    predict_proba返回的是一个 n 行 k 列的数组， 第 i 行 第 j 列上的数值是模型预测 第 i 个预测样本为某个标签的概率，并且每一行的概率和为1。
+# log_loss = metrics.log_loss(target, prob, labels=np.array([0,1,2,3,4])) # ValueError: The number of classes in labels is different from that in y_pred. Classes found in labels: [0 1 2 3 4]
+log_loss = metrics.log_loss(target, prob) # log loss =  0.004525287636016965
 print('log loss = ', log_loss)
